1f6e70c10604b9c00e5643204d00367986975cc7
Luciano Zangeronimo <zangeronimo@gmail.com>
2020-06-23 21:06:47 -0300

    add database.example.js

---
+++ backend/src/config/database.example.js
@@ -0,0 +1,13 @@
+const dbConfig = {
+    dialect: '',
+    host: '',
+    username: '',
+    password: '',
+    database: '',
+    define: {
+        timestamps: true,
+        underscored: true
+    }
+}
+
+module.exports = dbConfig;
