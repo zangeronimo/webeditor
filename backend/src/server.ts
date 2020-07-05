0639cb3d7032c626d045beab3750ee3ba024c5e3
Luciano Zangeronimo <zangeronimo@gmail.com>
2020-07-05 18:42:00 -0300

    not work on remote server, need test on local machine.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,12 @@
-import express from 'express'
-import '@controllers/UsersController'
+import express from 'express';
+import '@controllers/UsersController';
+import cors from 'cors';
 
-const app = express()
+const app = express();
+app.use(cors());
 
 app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' })
-})
+  return response.json({ message: 'Hello World' });
+});
 
-app.listen(3333)
+app.listen(4000);
--- backend/src/tests/FirstTest.spec.ts
+++ backend/src/tests/FirstTest.spec.ts
@@ -1,4 +1,5 @@
 import { User } from '@models/User'
+import axios from 'axios'
 
 test('it should be ok', () => {
   const user = new User()
@@ -6,4 +7,14 @@ test('it should be ok', () => {
   user.name = 'Diego'
 
   expect(user.name).toEqual('Diego')
+})
+
+test('it shold be ok get /sayhello url', async () => {
+  const result = await axios.get('http://localhost:4000/')
+
+  // check de result status
+  expect(result.status).toEqual(200)
+
+  // check de result body is "Hello World 2.0"
+  expect(result.data).toEqual('Hello World')
 })
