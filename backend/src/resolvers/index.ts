7112b0925abbd11f28f349853e45b63be8469bcf
Luciano Zangeronimo <zangeronimo@gmail.com>
2020-07-18 16:04:21 -0300

    refactory

--- backend/src/resolvers/index.ts
+++ backend/src/resolvers/index.ts
@@ -1,7 +1,7 @@
 import Query from './Query';
 import Mutation from './Mutation';
-const WebClient = require('./Type/WebClient');
-const WebTool = require('./Type/WebTool');
+const WebClient = require('./Type/System/WebClient');
+const WebTool = require('./Type/System/WebTool');
 
 export = {
     Query,
